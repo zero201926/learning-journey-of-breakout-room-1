2df2e21d3e65c387f2eeed621e66c2fb37d64f94
vikash <zero_transfer@outlook.com>
2019-11-12 11:55:43 +0000

    plane class

--- Airport/src/main/Plane.java
+++ Airport/src/main/Plane.java
@@ -1,4 +1,10 @@
 package main;
 
 public class Plane {
+    public Plane(){
+
+    }
+    public static void main(String[] args){
+
+    }
 }
--- Airport/src/test/PlaneTest.java
+++ Airport/src/test/PlaneTest.java
@@ -9,6 +9,7 @@ class PlaneTest {
     @Test
     public void PlaneIsMade(){
         main.Plane plane = new main.Plane();
-        
+        boolean var = plane instanceof main.Plane;
+        assertEquals(true, var);
     }
 }

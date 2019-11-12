d7b5995e4e9bc3be0536c533d9a01f8303bc55a7
vikash <zero_transfer@outlook.com>
2019-11-12 14:09:10 +0000

    first story

--- Airport/src/test/AirControlTest.java
+++ Airport/src/test/AirControlTest.java
@@ -1,6 +1,9 @@
 package test;
 
+import main.Plane;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,16 +11,20 @@ class AirControlTest {
 
     @Test
     public void PlaneLands(){
+        ArrayList<Plane> planedoublearray = new ArrayList<>();
 
+        main.Plane plane = new main.Plane();
+        main.Plane planedouble = new main.Plane();
+        main.AirControl airport = new main.AirControl(plane);
+        planedoublearray.add(planedouble);
+        airport.LandPlane(planedouble);
+//        assertArrayEquals(ArrayList<Plane>, planedouble);
+//        assertThat(garage, planedouble());
+        assertEquals(planedoublearray, airport.ViewGarage());
     }
 
     @Test
     public void PlaneTakesOff(){
-
-    }
-
-    @Test
-    public void PreventLandingWhenAirportFull(){
 
     }
 

d9e734c231bfb4045b62d3a02490d12ef1b83d2b
vikash <zero_transfer@outlook.com>
2019-11-12 11:08:32 +0000

    airport frame

---
+++ Airport/src/test/AirControlTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirControlTest {
+
+    @Test
+    public void PlaneLands(){
+
+    }
+
+    @Test
+    public void PlaneTakesOff(){
+
+    }
+
+    @Test
+    public void PreventLandingWhenAirportFull(){
+
+    }
+
+
+}
---
+++ Airport/src/test/PlaneTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaneTest {
+
+    @Test
+    public void PlaneIsMade(){
+        main.Plane plane = new main.Plane();
+        
+    }
+}

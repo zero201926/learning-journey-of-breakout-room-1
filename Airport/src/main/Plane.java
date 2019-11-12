f6740f8bd38431811eb4ab51be96d27bf5c65d94
Vikash Ramnarain <54401333+zero201926@users.noreply.github.com>
2019-11-12 14:26:36 +0000

    Update Plane.java

--- Airport/src/main/Plane.java
+++ Airport/src/main/Plane.java
@@ -2,3 +2,24 @@ package main;
 
 public class Plane {
 
+//    Class garage = [];
+    ArrayList<Plane> garage = new ArrayList<>();
+//    String[] garage = new String[100];
+    public AirControl(Plane plane){
+
+    }
+    public static void main(String[] args){
+
+    }
+
+    public void LandPlane(Plane planedouble){
+        garage.add(planedouble);
+    }
+
+    public ArrayList<Plane> ViewGarage(){
+        return garage;
+    }
+    public void TakeOfPlane(){
+
+    }
+}
